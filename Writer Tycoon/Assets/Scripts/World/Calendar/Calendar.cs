82c17da8ace92ea3dd190f0f4b32a928a092cdce
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-08-30 01:24:02 -0400

    Implemented months and years into the Calendar system with working incrementation

--- Writer Tycoon/Assets/Scripts/World/Calendar/Calendar.cs
+++ Writer Tycoon/Assets/Scripts/World/Calendar/Calendar.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using WriterTycoon.Input;
@@ -10,9 +11,44 @@ namespace WriterTycoon.World.Calendar
         [SerializeField] private CalendarData data;
         [SerializeField] private GameInputReader inputReader;
 
-        private int currentDay = 0;
+        public Dictionary<int, string> Months = new()
+        {
+            { 1, "January" },
+            { 2, "February" },
+            { 3, "March" },
+            { 4, "April" },
+            { 5, "May" },
+            { 6, "June" },
+            { 7, "July" },
+            { 8, "August" },
+            { 9, "September" },
+            { 10, "October" },
+            { 11, "November" },
+            { 12, "December" }
+        };
 
-        [SerializeField] private Text dayText;
+        public Dictionary<int, int> MonthDays = new()
+        {
+            { 1, 31 },
+            { 2, 28 },
+            { 3, 31 },
+            { 4, 30 },
+            { 5, 31 },
+            { 6, 30 },
+            { 7, 31 },
+            { 8, 31 },
+            { 9, 30 },
+            { 10, 31 },
+            { 11, 30 },
+            { 12, 31 }
+        };
+
+        private int currentDay;
+        private int currentMonth;
+        private int currentYear;
+        private int daysInMonth;
+
+        [SerializeField] private Text dateText;
         [SerializeField] private Text currentTimeText;
         [SerializeField] private Text currentSpeedText;
         VariableFrequencyTimer dayTimer;
@@ -20,22 +56,28 @@ namespace WriterTycoon.World.Calendar
         private void OnEnable()
         {
             inputReader.DefaultSpeed += SetDefaultSpeed;
-            inputReader.FasterSpeed += SetDoubleSpeed;
-            inputReader.FastestSpeed += SetQuintupleSpeed;
+            inputReader.FasterSpeed += SetFasterSpeed;
+            inputReader.FastestSpeed += SetFastestSpeed;
             inputReader.PauseCalendar += Pause;
         }
 
         private void OnDisable()
         {
             inputReader.DefaultSpeed -= SetDefaultSpeed;
-            inputReader.FasterSpeed -= SetDoubleSpeed;
-            inputReader.FastestSpeed -= SetQuintupleSpeed;
+            inputReader.FasterSpeed -= SetFasterSpeed;
+            inputReader.FastestSpeed -= SetFastestSpeed;
             inputReader.PauseCalendar -= Pause;
         }
 
         private void Start()
         {
-            currentDay = 0;
+            // Set the current date to January 1, 2024
+            currentDay = 15;
+            currentMonth = 12;
+            currentYear = 2023;
+
+            // Check for leap year
+            CheckForLeapYear();
 
             // Create the day timer
             dayTimer = new VariableFrequencyTimer(
@@ -50,7 +92,7 @@ namespace WriterTycoon.World.Calendar
             // Start the day timer
             dayTimer.Start();
 
-            dayText.text = $"Day: {currentDay}";
+            dateText.text = $"{Months[currentMonth]} {currentDay}, {currentYear}";
             currentTimeText.text = $"Current Time: {dayTimer.CurrentTime}";
             currentSpeedText.text = $"Current Speed: Default";
         }
@@ -66,12 +108,65 @@ namespace WriterTycoon.World.Calendar
             dayTimer.Dispose();
         }
 
+        /// <summary>
+        /// Pass a day on the Calendar
+        /// </summary>
         private void PassDay()
         {
+            bool newMonth = false;
+
+            // Increment the current day
             currentDay++;
-            dayText.text = $"Day: {currentDay}";
+
+            // Check if the current day surpasses the days in the month
+            if(currentDay > daysInMonth)
+            {
+                // Increment the current month
+                currentMonth++;
+
+                // Reset the first day of the month
+                currentDay = 1;
+
+                newMonth = true;
+            }
+
+            // Check if it is a new month
+            if(newMonth)
+            {
+                // Check if the current month surpasses the amount of months in a year
+                if (currentMonth > 12)
+                {
+                    // Increment the current year
+                    currentYear++;
+
+                    // Reset to the first month of the year
+                    currentMonth = 1;
+                }
+
+                // Check for a leap year
+                CheckForLeapYear();
+            }
+
+            dateText.text = $"{Months[currentMonth]} {currentDay}, {currentYear}";
         }
 
+        /// <summary>
+        /// Set the amount of days in the month according to if it is a Leap Year or not
+        /// </summary>
+        private void CheckForLeapYear()
+        {
+            // Check if it's February and a leap year
+            if (currentMonth == 2 && currentYear % 4 == 0)
+                // If so, set the amount of days to 29
+                daysInMonth = 29;
+            else
+                // Otherwise, look up in the dictionary for the amount of days
+                daysInMonth = MonthDays[currentMonth];
+        }
+
+        /// <summary>
+        /// Set the default calendar speed
+        /// </summary>
         private void SetDefaultSpeed()
         {
             // Set the default speed for the timer
@@ -80,7 +175,10 @@ namespace WriterTycoon.World.Calendar
             currentSpeedText.text = $"Current Speed: Default";
         }
 
-        private void SetDoubleSpeed()
+        /// <summary>
+        /// Set the faster calendar speed
+        /// </summary>
+        private void SetFasterSpeed()
         {
             // Set the default speed for the timer
             dayTimer.SetFasterSpeed();
@@ -88,7 +186,10 @@ namespace WriterTycoon.World.Calendar
             currentSpeedText.text = $"Current Speed: Faster";
         }
 
-        private void SetQuintupleSpeed()
+        /// <summary>
+        /// Set the fastest calendar speed
+        /// </summary>
+        private void SetFastestSpeed()
         {
             // Set the default speed for the timer
             dayTimer.SetFastestSpeed();
@@ -96,6 +197,9 @@ namespace WriterTycoon.World.Calendar
             currentSpeedText.text = $"Current Speed: Fastest";
         }
 
+        /// <summary>
+        /// Pause the calendar
+        /// </summary>
         private void Pause()
         {
             // Check if the Timer is running
