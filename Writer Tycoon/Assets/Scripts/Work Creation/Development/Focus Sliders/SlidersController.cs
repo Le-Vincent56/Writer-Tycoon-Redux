860d0e125b915157ae500634ccba5adf97d3747f
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-09-23 20:59:00 -0400

    Tried getting slider clamping to work

---
+++ Writer Tycoon/Assets/Scripts/Work Creation/Development/Focus Sliders/SlidersController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using WriterTycoon.WorkCreation.UI.Development;
+
+namespace WriterTycoon.WorkCreation.Development.FocusSliders
+{
+    public class SlidersController : MonoBehaviour
+    {
+        [SerializeField] private Slider[] sliders;
+        [SerializeField] private int[] previousValues;
+
+        private void Awake()
+        {
+            // Attempt to get children Focus Sliders
+            FocusSlider[] focusSliders = GetComponentsInChildren<FocusSlider>();
+
+            // Exit case - if no Focus Sliders are found
+            if (focusSliders == null) return;
+
+            List<Slider> sliderList = new();
+            List<int> initialValues = new();
+
+            // Iterate through each Slider
+            foreach (FocusSlider slider in focusSliders)
+            {
+                // Get the child slider
+                Slider childSlider = slider.GetComponentInChildren<Slider>();
+
+                // Add the child slider to the list
+                sliderList.Add(childSlider);
+
+                // Add the initial slider value to the list
+                initialValues.Add((int)childSlider.value);
+            }
+
+            // Cast the lists to arrays for more clear traversal
+            sliders = sliderList.ToArray();
+            previousValues = initialValues.ToArray();
+        }
+
+        private void Start()
+        {
+            for(int i = 0; i < sliders.Length; i++)
+            {
+                sliders[i].onValueChanged.AddListener((value) => BindSliders(value, i));
+            }
+        }
+
+        /// <summary>
+        /// Callback function to bind the sliders to a maximum
+        /// </summary>
+        /// <param name="value"></param>
+        private void BindSliders(float value, int index)
+        {
+            // TODO: Get a reference to the slider being changed here
+
+            // Calculate the total value
+            int totalValue = CalculateTotalValue();
+
+            // Check if the total value is above the maximum of 15
+            if (totalValue > 15)
+            {
+                // Calculate the excess value
+                float excessValue = totalValue - 15;
+
+                // Subtract the excess from the slider's previous value
+                sliders[index].value = previousValues[index] - excessValue;
+            }
+
+            // Update the slider's previous value
+            previousValues[index] = (int)sliders[index].value;
+        }
+
+        /// <summary>
+        /// Calculate the total value between all of the Sliders
+        /// </summary>
+        private int CalculateTotalValue()
+        {
+            // Create a container for the total value for an initial check
+            int totalValue = 0;
+
+            // Iterate through each Slider
+            foreach (Slider slider in sliders)
+            {
+                // Add each slider value
+                totalValue += (int)slider.value;
+            }
+
+            return totalValue;
+        }
+    }
+}
