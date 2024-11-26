2b0b2f8cd64b472db8e53b86d4f31c5c04b226ec
Vincent Le <89761643+Le-Vincent56@users.noreply.github.com>
2024-11-26 13:56:25 -0500

    Namespace cleanup

--- Writer Tycoon/Assets/Scripts/Entities/Competitors/Learning/CompetitorBrain.cs
+++ Writer Tycoon/Assets/Scripts/Entities/Competitors/Learning/CompetitorBrain.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using GhostWriter.WorkCreation.Development.PointGeneration;
 using GhostWriter.WorkCreation.Ideation.Audience;
